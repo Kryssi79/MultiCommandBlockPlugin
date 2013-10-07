13c2f6a6c1f35a5fcd7874ed04dd6eba4ae3fedd
Kryssi79 <KryssiListy@freenet.de>
2013-10-07 13:46:43 +0200

    1.  Test-Command  =>  /mcbTest

--- src/com/MeLxKry/mcbp/main_MCBP.java
+++ src/com/MeLxKry/mcbp/main_MCBP.java
@@ -6,9 +6,9 @@ package com.MeLxKry.mcbp;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public class main_MCBP extends JavaPlugin 
 {
-
 	// public PluginDescriptionFile pdf = this.getDescription();
 	
 	// public PluginManager pluginMan;
@@ -20,17 +20,20 @@ public class main_MCBP extends JavaPlugin
         // getLogger().info(pdf.getName() + " v" + pdf.getVersion() + " Enabled!");
         System.out.println("Starte: Multi Command Block Plugin ...  ");
         
+        this.getCommand("mcbTest").setExecutor(new testCommand());
+        
         // pluginMan = getServer().getPluginManager();
-
         // pluginMan.registerEvents(listener, this);
    }
     
    
     
+    
     @Override
    public void onDisable() {
     	System.out.println("beende:  Multi Command Block Plugin ");
    }
+    
     
     
 }
--- src/com/MeLxKry/mcbp/testCommand.java
+++ src/com/MeLxKry/mcbp/testCommand.java
@@ -10,7 +10,11 @@ public class testCommand implements CommandExecutor
 
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String strLabel, String[] args) {
-		
+		if(args.length == 0)
+		{
+			cs.sendMessage("test-Command   OK ");
+			return true;
+		}
 		
 		return false;
 	}
