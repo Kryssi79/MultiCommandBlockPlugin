f0eadf6522b497ea40c60bb5fe218892d8eacda7
Kryssi79 <KryssiListy@freenet.de>
2013-10-07 21:57:03 +0200

    sendeBefehl()  Methode

--- src/com/MeLxKry/mcbp/CommandBlockExt.java
+++ src/com/MeLxKry/mcbp/CommandBlockExt.java
@@ -1,11 +1,14 @@
 package com.MeLxKry.mcbp;
 
 import org.bukkit.block.*;
+import org.bukkit.command.CommandSender;
 
-public class CommandBlockExt {
+public class CommandBlockExt 
+{
 	main_MCBP plugin;
 	CommandBlock m_CommandBlock;
 	String m_CommandsString;
+	
 	
 	public CommandBlockExt(main_MCBP plugin, BlockState state) {
 		this.plugin = plugin;
@@ -16,9 +19,26 @@ public class CommandBlockExt {
 		getCommand();
 	}
 	
+	
 	private void getCommand()
 	{
+		// oder über   org.bukkit.command.CommandExecutor  
 		m_CommandsString = m_CommandBlock.getCommand();
+		sendeBefehl("say hello World");
+	}
+	
+	
+	private boolean sendeBefehl(String sCmd)
+	{
+		return sendeBefehl(plugin.getServer().getConsoleSender(), sCmd);
+	}
+	
+	private boolean sendeBefehl(CommandSender oSender, String sCmd)
+	{
+		// org.bukkit.command.CommandExecutor  
+		// getServer().dispatchCommand(getServer().getConsoleSender(), "BEFEHL");
+		plugin.getServer().dispatchCommand(oSender, sCmd);
+		return true;
 	}
 	
 }
