c18ee306db0c99910bab6fa100486a6877f4d159
memoryleakx <mikekrueger81@gmail.com>
2013-10-07 21:15:35 +0200

    new CommandBlockExt class

--- src/com/MeLxKry/mcbp/CommandBlockExt.java
+++ src/com/MeLxKry/mcbp/CommandBlockExt.java
@@ -1,189 +1,24 @@
 package com.MeLxKry.mcbp;
 
-import java.util.List;
-
-import org.bukkit.Chunk;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.*;
-import org.bukkit.material.MaterialData;
-import org.bukkit.metadata.MetadataValue;
-import org.bukkit.plugin.Plugin;
 
-public class CommandBlockExt implements CommandBlock {
+public class CommandBlockExt {
 	main_MCBP plugin;
-	private String m_currentCommandString;
+	CommandBlock m_CommandBlock;
+	String m_CommandsString;
 	
-	public CommandBlockExt(main_MCBP plugin) {
+	public CommandBlockExt(main_MCBP plugin, BlockState state) {
 		this.plugin = plugin;
+		if (state instanceof CommandBlock)
+		{
+			m_CommandBlock = (CommandBlock)state;
+		}
+		getCommand();
 	}
 	
-	@Override
-	public Block getBlock() {
-		// TODO Auto-generated method stub
-		return null;
+	private void getCommand()
+	{
+		m_CommandsString = m_CommandBlock.getCommand();
 	}
-
-	@Override
-	public Chunk getChunk() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public MaterialData getData() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public byte getLightLevel() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Location getLocation() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Location getLocation(Location arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public byte getRawData() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Material getType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getTypeId() {
-		// TODO Auto-generated method stub
-		return 137;
-	}
-
-	@Override
-	public World getWorld() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getZ() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setData(MaterialData arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setRawData(byte arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setType(Material arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean setTypeId(int arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean update() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean update(boolean arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean update(boolean arg0, boolean arg1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public List<MetadataValue> getMetadata(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean hasMetadata(String arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void removeMetadata(String arg0, Plugin arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setMetadata(String arg0, MetadataValue arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public String getCommand() {
-		return m_currentCommandString;
-	}
-
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setCommand(String arg0) {
-		m_currentCommandString = arg0;
-		
-	}
-
-	@Override
-	public void setName(String arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 }
