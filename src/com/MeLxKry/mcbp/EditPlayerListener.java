4adcdabd898b5750bcc96ead65e2dc84b99ff327
Kryssi79 <KryssiListy@freenet.de>
2013-10-07 02:12:59 +0200

    add Listener

---
+++ src/com/MeLxKry/mcbp/EditPlayerListener.java
@@ -0,0 +1,24 @@
+package com.MeLxKry.mcbp;
+
+// import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+// import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+// import org.bukkit.event.player.PlayerJoinEvent;
+// import org.bukkit.block.Sign;
+
+
+public class EditPlayerListener implements Listener 
+{
+
+	
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		Player player = event.getPlayer();
+		// Block block = event.getClickedBlock();
+		player.sendMessage("  Interact ");
+	}
+	
+}
--- src/com/MeLxKry/mcbp/main_MCBP.java
+++ src/com/MeLxKry/mcbp/main_MCBP.java
@@ -1,5 +1,9 @@
 package com.MeLxKry.mcbp;
-import org.bukkit.plugin.PluginDescriptionFile;
+
+// import java.io.IOException;
+
+//import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -7,17 +11,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class main_MCBP extends JavaPlugin 
 {
 
-	public PluginDescriptionFile pdf = this.getDescription();
+	// public PluginDescriptionFile pdf = this.getDescription();
+	
+	public PluginManager pluginMan;
+	private EditPlayerListener listener;
+	
     
     @Override
    public void onEnable(){
         // getLogger().info(pdf.getName() + " v" + pdf.getVersion() + " Enabled!");
         System.out.println("Starte: Multi Command Block Plugin ...  ");
+        
+        pluginMan = getServer().getPluginManager();
+
+        pluginMan.registerEvents(listener, this);
    }
+    
    
+    
     @Override
    public void onDisable() {
     	System.out.println("beende:  Multi Command Block Plugin ");
    }
+    
     
 }
