9ad2367b4920d19f84ceb398087ca115cffe6be9
Kryssi79 <KryssiListy@freenet.de>
2013-10-13 19:29:17 +0200

    ParserLoop extends CommandParser

--- src/com/MeLxKry/mcbp/parser/ParserLoop.java
+++ src/com/MeLxKry/mcbp/parser/ParserLoop.java
@@ -1,5 +1,7 @@
 package com.MeLxKry.mcbp.parser;
 
+import org.bukkit.entity.Player;
+
 
 public class ParserLoop extends CommandParser  
 {
@@ -10,6 +12,15 @@ public class ParserLoop extends CommandParser
 		this.m_Parent = myParent;
 	}
 	
+	protected void Parse(ParsedCommand parsedCommand)
+	{
+		if (parsedCommand.getCommand().contains(" LOOP:"))
+		{
+			if(m_Parent.bLogToConsole==true)  { System.out.println(" Parser LOOP  "); }
+			
+		}
+		
+	}
 	
 
 }
