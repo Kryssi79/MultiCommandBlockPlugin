57b2a54be41bf02c34724bcdd401d7ab5c6d1073
Kryssi79 <KryssiListy@freenet.de>
2013-10-13 19:15:12 +0200

    ParserLoop  -  Anfang

---
+++ src/com/MeLxKry/mcbp/parser/ParserLoop.java
@@ -0,0 +1,15 @@
+package com.MeLxKry.mcbp.parser;
+
+
+public class ParserLoop extends CommandParser  
+{
+	
+	CommandParser m_Parent;
+	
+	public ParserLoop(CommandParser myParent) {
+		this.m_Parent = myParent;
+	}
+	
+	
+
+}
